8c9bdf9f446982ee2b358be80f316f29aac1ec7a
wwwStereo <voronone3@gmail.com>
2022-11-28 00:03:05 +0300

    kod fibanachi

---
+++ main.py
@@ -0,0 +1,11 @@
+n = int(input('Вводится: '))
+
+def F(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return F(n-1)+F(n-2)
+
+print('Выводится:', F(n))
